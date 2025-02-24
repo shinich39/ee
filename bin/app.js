37087f8be0bf173d33a56c5660df3f64a71bc1b6
shinich39 <shinich39@gmail.com>
2025-02-25 01:31:34 +0900

    fix

--- bin/app.js
+++ bin/app.js
@@ -12,5 +12,5 @@ mongoose.connect(`mongodb://root:1234@mongo:27017/`, {
   authSource: "admin"
 });
 
-app.get('/', (req, res) => res.send('Hello, docker!'));
+app.get('/', (req, res) => res.send('Hello, docker!!'));
 app.listen(3000, () => console.log('[express] running on port 3000'));
